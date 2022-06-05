c8a34eecda0a03f389c5a612354c1ab39bbb7083
Emily Reed <emilyreed89@gmail.com>
2022-06-04 22:50:05 -0400

    updated outlander object

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const outlander = {
             'hairColor': 'red'
         },
         'occupation': ['soldier', 'laird', "Propietor of Fraser's Ridge", 'Printer'],
+        'quote': "For if you feel for me as I do for you–then I am asking you to tear out your heart and live without it.",
         'image': 'https://static.wikia.nocookie.net/outlander/images/d/d9/S5-Jamie.jpg/revision/latest/scale-to-width-down/328?cb=20200222025214'
 
     },
@@ -29,9 +30,10 @@ const outlander = {
             'hairColor': 'curly light brown'
         },
         'occupation': 'Physician',
+        'quote':  "All right you bloody Scottish bastard, let’s see how stubborn you really are.",
         'image': 'https://static.wikia.nocookie.net/outlander/images/6/68/S06E03-still15.jpg/revision/latest/scale-to-width-down/333?cb=20220320171422'
     },
-    'brianna macKenzie': {
+    'brianna mackenzie': {
         'name': 'Brianna MacKenzie',
         'birthDate': 'November 23, 1948',
         'birthPlace': 'Boston, Massachusetts, USA',
@@ -41,8 +43,61 @@ const outlander = {
             'hairColor': 'red'
         },
         'occupation': ['Engineer', 'Plant Inspector for North of Scotland'],
+        'quote' : "I've seen a marriage made from obligation and one made for love. If I hadn't seen both, I could have lived with obligation. But I have seen both... and I won't.",
         'image': 'https://static.wikia.nocookie.net/outlander/images/8/85/S5-Brianna.jpg/revision/latest/scale-to-width-down/1000?cb=20200222014639'
-    }  
+    },
+    'jonathon randall': {
+        'name': 'Jonathon Randall',
+        'birthDate': 'September 3, 1705',
+        'birthPlace': 'unknown',
+        'features' : {
+            'height': "5'10",
+            'eyeColor': 'hazel',
+            'hairColor': 'dark brown'
+        },
+        'occupation': "Captain of His Majesty's Eighth Dragoons",
+        'quote' : "I dwell in darkness, Madam, and darkness is where I belong.",
+        'image': 'https://static.wikia.nocookie.net/outlander/images/a/a8/BJR-Season2.jpg/revision/latest/scale-to-width-down/350?cb=20160609133755'
+    },
+    'frank randall': {
+        'name': 'Frank Randall',
+        'birthDate': '1906',
+        'birthPlace': 'unknown',
+        'features' : {
+            'height': "5'10",
+            'eyeColor': 'hazel',
+            'hairColor': 'dark brown'
+        },
+        'occupation': ['Historian', 'Officer in the British Army'],
+        'quote' : "I love you, Claire. Unconditionally. No matter what.",
+        'image': 'https://static.wikia.nocookie.net/outlander/images/a/ad/3_Frank_648x1080.jpg/revision/latest/scale-to-width-down/300?cb=20170721030520'
+    },
+    'john grey': {
+        'name': 'John Grey',
+        'birthDate': 'June 14, 1729',
+        'birthPlace': 'unknown',
+        'features' : {
+            'height': "5'6",
+            'eyeColor': 'light blue',
+            'hairColor': 'blond'
+        },
+        'occupation': ['Soldier', 'Diplomat', 'Governor'],
+        'quote' : "Do you know what is is to love someone, and never--Never!--Be able to give them peace, or joy, or happiness?",
+        'image': 'https://static.wikia.nocookie.net/outlander/images/a/a7/S5-Lord-John.jpg/revision/latest/scale-to-width-down/318?cb=20200206031052'
+    },
+    'roger mackenzie': {
+        'name': 'Roger Jerimiah Wakefield MacKenzie',
+        'birthDate': '1940 or 1941',
+        'birthPlace': 'unknown',
+        'features' : {
+            'height': "6'3",
+            'eyeColor': 'green',
+            'hairColor': 'black'
+        },
+        'occupation': ['Historian', 'Minister'],
+        'quote' : "Brianna, I love you all, or not at all.",
+        'image': 'https://static.wikia.nocookie.net/outlander/images/e/e9/Roger-S6.jpg/revision/latest/scale-to-width-down/321?cb=20220514184539'
+    },     
 }
 
 app.get('/', (req, res) => {
